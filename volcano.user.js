e0b3a3176cc6600e2208b2167d4c30e04420d8c8
dyr14n <164554009+dyr14n@users.noreply.github.com>
2025-09-15 16:09:49 +0700

    Create volcano.user.js

---
+++ volcano.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Auto Fully Bypass Volcano
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Auto Fully Bypass Volcano
+// @author       DyRian (based on IHaxU)
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @require      https://github.com/dyr14n/bypass/raw/main/bypassvolcano.user.js
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
+// ==/UserScript==
