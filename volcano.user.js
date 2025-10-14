515b887f9b5f5b96fb117a314a38cf1512401005
dyr14n <164554009+dyr14n@users.noreply.github.com>
2025-10-14 17:47:14 +0700

    Update volcano.user.js

--- volcano.user.js
+++ volcano.user.js
@@ -7,7 +7,6 @@
 // @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
 // @run-at       document-start
-// @grant        unsafeWindow
 // @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
 // @downloadURL  https://github.com/dyr14n/bypass/raw/main/volcano.user.js
 // @updateURL    https://github.com/dyr14n/bypass/raw/main/volcano.user.js
