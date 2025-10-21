d1e4c565eae329a0471ab6021fd5a12e78da5bd6
dyr14n <164554009+dyr14n@users.noreply.github.com>
2025-10-22 03:24:14 +0700

    Update volcano.user.js

--- volcano.user.js
+++ volcano.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Auto Bypass Volcano
 // @namespace    http://tampermonkey.net/
-// @version      1.6.1.3
+// @version      1.6.1.4
 // @description  You only need to solve the captcha
 // @author       DyRian (based on IHaxU)
 // @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
 // @run-at       document-start
+// @grant        unsafeWindow
 // @icon         https://i.postimg.cc/y6Gx5NBD/volcano.png
 // @downloadURL  https://github.com/dyr14n/bypass/raw/main/volcano.user.js
 // @updateURL    https://github.com/dyr14n/bypass/raw/main/volcano.user.js
