a8b79ae6df14a7ec62112c072b6f2733effcd123
dyr14n <164554009+dyr14n@users.noreply.github.com>
2025-10-22 03:15:50 +0700

    Create alternative.user.js

---
+++ alternative.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Dyrian Bypass
+// @namespace    Brave User
+// @version      1.6.1.4
+// @description  You only need to solve the captcha
+// @author       DyRian (based on IHaxU)
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @grant        unsafeWindow
+// @run-at       document-start
+// @downloadURL  https://github.com/dyr14n/bypass/raw/main/alternative.user.js
+// @updateURL    https://github.com/dyr14n/bypass/raw/main/alternative.user.js
+// @require      https://github.com/dyr14n/bypass/raw/main/braveuser.js
+// ==/UserScript==
