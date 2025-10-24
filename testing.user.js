fa03c275a334f6bfecac90a58f85d48233800cf1
dyr14n <164554009+dyr14n@users.noreply.github.com>
2025-10-24 16:20:51 +0700

    Update and rename alternative.user.js to testing.user.js

--- testing.user.js
+++ testing.user.js
@@ -8,7 +8,7 @@
 // @match        https://work.ink/*
 // @grant        unsafeWindow
 // @run-at       document-start
-// @downloadURL  https://github.com/dyr14n/bypass/raw/main/alternative.user.js
-// @updateURL    https://github.com/dyr14n/bypass/raw/main/alternative.user.js
-// @require      https://github.com/dyr14n/bypass/raw/main/braveuser.js
+// @downloadURL  https://github.com/dyr14n/bypass/raw/main/testing.user.js
+// @updateURL    https://github.com/dyr14n/bypass/raw/main/testing.user.js
+// @require      https://github.com/dyr14n/bypass/raw/main/testing.js
 // ==/UserScript==
