944ab1a1fa71467825755cb4e47b131921be9040
dyr14n <164554009+dyr14n@users.noreply.github.com>
2025-10-26 22:55:47 +0700

    Update volcano.user.js

--- volcano.user.js
+++ volcano.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Bypass Volcano
 // @namespace    http://tampermonkey.net/
-// @version      1.6.2.2
+// @version      1.6.2.3
 // @description  You only need to solve the captcha
 // @author       DyRian (based on IHaxU)
 // @match        https://key.volcano.wtf/*
