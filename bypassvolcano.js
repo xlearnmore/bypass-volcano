acd741cc218eff57ee55225a8cdbff1442eb842e
Xlearnmore <136180862+xlearnmore@users.noreply.github.com>
2025-10-30 02:50:11 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- bypassvolcano.js
+++ bypassvolcano.js
@@ -20,7 +20,7 @@
             bypassSuccess: "Bypass successful, waiting {time}s...",
             backToCheckpoint: "Returning to checkpoint...",
             captchaSuccessBypassing: "CAPTCHA solved successfully, bypassing...",
-            version: "Version v1.6.2.6",
+            version: "Version v1.6.2.7",
             madeBy: "Rework by Xlearnmore (based on IHaxU & DyRian)"
         }
     };
@@ -884,3 +884,4 @@ function createDestinationProxy() {
         ob.observe(document.documentElement, { childList: true, subtree: true });
     }
 })();
+
